ffa06ecb1a830ee69fc2f3e23e1a63cd53b8c770
Aaron <atbrown89@gmail.com>
2022-08-04 16:51:53 -0700

    components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/App.css';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+// import { connect } from "react-redux";
+// TODO : set up navigation upon successful login
+// import { useNavigate } from "react-router-dom";
+
+const LoginPage = (props) => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: build out handling
+    }
+
+  return (
+    <div className="login">
+      <h1>Login Page</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="login-field">
+          <label hmtlFor="username">Username:</label><br />
+          <input type="text" id="username" name="username" placeholder="enter username" ref={usernameRef} />
+        </div>
+        <div className="login-field">
+          <label htmlFor="pwd">Password:</label><br />
+          <input type="password" id="pwd" name="pwd" placeholder="enter password" ref={passwordRef} />
+        </div>
+        <div className="login-field">
+          <button className="btn" type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};  
+
+export default LoginPage;
