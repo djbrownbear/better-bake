508436695eeece60685160fe10e759ec9f96d449
Aaron <atbrown89@gmail.com>
2022-08-15 11:26:40 -0700

    test:add _Data test

---
+++ src/tests/_Data.test.js
@@ -0,0 +1,125 @@
+const { _saveQuestion, _saveQuestionAnswer } = require("../utils/_DATA");
+
+describe("_saveQuestion", () => {
+  it("should return true with all correct parameters", async () => {
+    const optionOneText = "More team outings";
+    const optionTwoText = "Days of service";
+    const author = "tylermcginnis";
+
+   const res = await _saveQuestion(
+      {optionOneText, 
+       optionTwoText, 
+       author,
+      }
+    );
+
+    expect(res).toBeTruthy();
+  });
+
+  it("should display error if optionOneText is invalid", async () => {
+    const optionOneText = undefined;
+    const optionTwoText = "Days of service";
+    const author = "tylermcginnis";
+
+    const res = await _saveQuestion(
+        {optionOneText, 
+         optionTwoText, 
+         author,
+        }
+      ).catch(e => e);
+
+    expect(res).toBe("Please provide optionOneText, optionTwoText, and author");
+  });
+
+  it("should display error if optionTwoText is invalid", async () => {
+    const optionOneText = "More team outings";
+    const optionTwoText = undefined;
+    const author = "tylermcginnis";
+
+    const res = await _saveQuestion(
+        {optionOneText, 
+         optionTwoText, 
+         author,
+        }
+      ).catch(e => e);
+
+    expect(res).toBe("Please provide optionOneText, optionTwoText, and author");
+  });
+
+  it("should display error if author is invalid", async () => {
+    const optionOneText = "More team outings";
+    const optionTwoText = "Days of service";
+    const author = undefined;
+
+    const res = await _saveQuestion(
+        {optionOneText, 
+         optionTwoText, 
+         author,
+        }
+      ).catch(e => e);
+
+    expect(res).toBe("Please provide optionOneText, optionTwoText, and author");
+  });
+})
+
+describe("_saveQuestionAnswer", () => {
+  it("should return true with all correct parameters", async () => {
+    const user = "tylermcginnis";
+    const id = "vthrdm985a262al8qx3do";
+    const ans = "optionOne";
+
+   const res = await _saveQuestionAnswer(
+      {authedUser: user, 
+       qid: id, 
+       answer: ans,
+      }
+    );
+
+    expect(res).toBeTruthy();
+  });
+
+  it("should display error if authedUser is invalid", async () => {
+    const user = undefined;
+    const id = "vthrdm985a262al8qx3do";
+    const ans = "optionOne";
+
+   const res = await _saveQuestionAnswer(
+      {authedUser: user, 
+       qid: id, 
+       answer: ans,
+      }
+    ).catch(e => e);
+
+    expect(res).toBe("Please provide authedUser, qid, and answer");
+  });
+
+  it("should display error if qid is invalid", async () => {
+    const user = "tylermcginnis";
+    const id = undefined;
+    const ans = "optionOne";
+
+   const res = await _saveQuestionAnswer(
+      {authedUser: user, 
+       qid: id, 
+       answer: ans,
+      }
+    ).catch(e => e);
+
+    expect(res).toBe("Please provide authedUser, qid, and answer");
+  });
+
+  it("should display error if ans is invalid", async () => {
+    const user = "tylermcginnis";
+    const id = "vthrdm985a262al8qx3do";
+    const ans = undefined;
+
+   const res = await _saveQuestionAnswer(
+      {authedUser: user, 
+       qid: id, 
+       answer: ans,
+      }
+    ).catch(e => e);
+
+    expect(res).toBe("Please provide authedUser, qid, and answer");
+  });
+})
