a6ef595fe2941b7ac4216ddacc7f985403cb1e1e
Aaron <atbrown89@gmail.com>
2022-08-15 23:50:50 -0700

    style:default avatar

--- src/components/PollHeader.js
+++ src/components/PollHeader.js
@@ -1,10 +1,12 @@
 import { formatDate } from "../utils/helpers";
 
 const PollHeader = ({ avatar, name, timestamp}) => {
+  // const defaultAvatar = "../assets/images/avatars/icons8-avatar-67.png";
+  const defaultAvatar = "https://img.icons8.com/external-others-inmotus-design/67/000000/external-Avatar-round-icons-others-inmotus-design-5.png";
 
   return (
     <div className="poll-header">
-      <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+      <img src={avatar ? avatar : defaultAvatar} alt={`Avatar of ${name}`} className="avatar" />
       <h2>Would you rather...</h2>
       <span>{ formatDate(timestamp) }</span>
     </div>
