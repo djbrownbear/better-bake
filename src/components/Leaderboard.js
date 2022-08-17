7d4ec5839090c76d8af4ba85017e4f8b4c1a05dd
Aaron <atbrown89@gmail.com>
2022-08-16 23:39:00 -0700

    feat:build out leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,53 @@
 import { connect } from "react-redux";
 
-const Leaderboard = () => {
+const Leaderboard = ({ usersList }) => {
+  const defaultAvatar = "https://img.icons8.com/external-others-inmotus-design/67/000000/external-Avatar-round-icons-others-inmotus-design-5.png";
+
   return (
     <div>
       <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Created</th>
+            <th>Answered</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+            usersList.map((user) => (
+              <tr key={user.id}>
+                <td>
+                  <div className="avatar-wrapper leaderboard">
+                    <img 
+                      src={user.avatarURL ? user.avatarURL : defaultAvatar }
+                      alt={`Avatar of ${user.name}`} 
+                    />
+                    <span>{user.id}</span>
+                  </div>
+                </td>
+                <td>{user.questions.length}</td>
+                <td>{Object.keys(user.answers).length}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default connect()(Leaderboard);
+const mapStateToProps = ({ users, authedUser}) => {
+
+  const countAskedAnswered = (x) => {
+    const asked = x.questions.length;
+    const answered = Object.keys(x.answers).length;
+    return asked + answered;
+  }
+
+  return {
+    usersList: Object.values(users).sort((a,b) => countAskedAnswered(b) - countAskedAnswered(a)),
+  };
+}
+
+export default connect(mapStateToProps)(Leaderboard);
