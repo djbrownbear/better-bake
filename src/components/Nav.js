cc04d187f5260890363c38b2679a9c368d224994
Aaron <atbrown89@gmail.com>
2022-08-28 23:58:11 -0700

    style: navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { logoutAuthedUser } from "../actions/authedUser";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin,  } from "@fortawesome/free-brands-svg-icons";
+import { faFolder, faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 
 
 const Nav = ({ dispatch, authedUser, user }) => {
@@ -26,38 +28,46 @@ const Nav = ({ dispatch, authedUser, user }) => {
   return (
     <nav className="nav" id="myTopnav">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-      <ul className="desktop-menu-main">
-        <li>
-          <Link to="/"><img className="app-logo" src="https://img.icons8.com/emoji/48/000000/ballot-box-with-ballot.png" alt="Voting Ballot Box" /></Link>
-        </li>
-        <li>
-          <Link to="/add">New Poll</Link>
-        </li>
-        <li>
-          <Link to="/leaderboard">Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/auth">Switch User</Link>
-        </li>
-        <button className="icon" type="button" onClick={toggleNav}><i className="fa fa-bars"></i></button>
-      </ul>
-      {!authedUser &&
-        <ul className="desktop-menu-login">
-          {/* TODO - ADD FONT AWESOME ICONS, FIX FORMATTING */}
-          <li><a href="https://github.com/djbrownbear" target="_blank" rel="noreferrer" alt="link to github">LinkedIn</a></li>
-          <li><a href="https://www.linkedin.com/in/aarontimothybrown/" target="_blank" rel="noreferrer" alt="link to github">GitHub</a></li>
-          <li><a href="https://aaron.aaronandanita.com" target="_blank" rel="noreferrer" alt="link to portfolio">Portfolio</a></li>
-        </ul>
-      }
-      {authedUser &&
-        <ul className="avatar-wrapper nav-avatar">
+      <div className="logo-wrapper">
+        <Link to="/">
+          <img 
+            className="app-logo" 
+            src="https://img.icons8.com/emoji/48/000000/ballot-box-with-ballot.png" 
+            alt="Voting Ballot Box" 
+          />
+        </Link>
+      </div>    
+      <div className="site-menu-inner">
+        <div className="top-bar">
+         <button className="icon" type="button" onClick={toggleNav}><FontAwesomeIcon icon={faXmarkCircle}/></button>
+        </div>
+        <ul className="desktop-menu-main">
           <li>
-            <img src={user.avatarURL} alt={`Avatar of ${user.name}`} className="avatar" />
-            <span>{user.id}</span>
-            <button data-testid="logout-button" className="btn btn-logout" type="button" onClick={handleClick}>Logout</button>
+            <Link to="/add">New Poll</Link>
+          </li>
+          <li>
+            <Link to="/leaderboard">Leaderboard</Link>
+          </li>
+          <li>
+            <Link to="/auth">Switch User</Link>
           </li>
         </ul>
-      }
+        <button className="icon close" type="button" onClick={toggleNav}><i className="fa fa-bars"></i></button>
+        {!authedUser &&
+          <ul className="desktop-menu-social">
+            <li><a href="https://github.com/djbrownbear" target="_blank" rel="noreferrer" alt="link to github"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            <li><a href="https://www.linkedin.com/in/aarontimothybrown/" target="_blank" rel="noreferrer" alt="link to github"><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a href="https://aaron.aaronandanita.com" target="_blank" rel="noreferrer" alt="link to portfolio"><FontAwesomeIcon icon={faFolder} /></a></li>
+          </ul>
+        }  
+        {authedUser &&
+          <div className="avatar-wrapper nav-avatar">
+              <img src={user.avatarURL} alt={`Avatar of ${user.name}`} className="avatar" />
+              <span>{user.id}</span>
+              <button data-testid="logout-button" className="btn btn-logout" type="button" onClick={handleClick}>Logout</button>
+          </div>
+        }
+      </div>
     </nav>
   );
 }
