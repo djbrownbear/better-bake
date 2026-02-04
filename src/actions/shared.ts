93fe55ad2a7de2b3754b758433d8f989f3571c11
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2026-02-04 01:02:14 -0800

    refactor: convert Redux actions to TypeScript
    - Migrate all 5 action files to TypeScript
    - Type thunk actions with ThunkAction<Promise<void>, RootState, unknown, AnyAction>
    - Add proper parameter types to all action creators
    - Type handleAddPoll, handleAddAnswer, handleInitialData thunks
    - Export action type constants as const

--- src/actions/shared.ts
+++ src/actions/shared.ts
@@ -3,9 +3,11 @@ import { receiveUsers } from "./users";
 import { receivePolls } from "./polls";
 import { receiveBakers } from "./bakers";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
+import { ThunkAction } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { RootState } from '../types';
 
-
-export function handleInitialData () {
+export function handleInitialData(): ThunkAction<Promise<void>, RootState, unknown, AnyAction> {
   return (dispatch) => {
     dispatch(showLoading());
     return getInitialData().then(({users, polls, bakers}) => {
